bb527b4a2a53c3dfb3b0132843544b49948b202e
Falekin7 <falekinldg@gmail.com>
2025-05-11 21:30:28 -0300

    modif design

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,26 @@ import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
   return (
-    <>
-      <ThemeProvider>
-        <div className="flex flex-col items-center justify-center min-h-screen bg-white dark:bg-black dark:text-white">
-          <h1 className="text-3xl font-bold mb-3">My website</h1>
-          <p>Click the button to toggle dark mode!</p>
-          <div className="mt-5">
+    <ThemeProvider>
+      <div className="min-h-screen bg-[#e4e4e4] dark:bg-[#06070b]">
+        <header className="flex items-center justify-between px-24 py-4  bg-[#e4e4e4] dark:bg-[#06070b] dark:text-white">
+          <div className="text-lg font-semibold">Lucio Morales</div>
+          <nav
+            className="
+    flex gap-8 text-xl font-medium px-10 py-4 rounded-full transition-all duration-300
+  "
+          >
+            <a href="#">Proyectos</a>
+            <a href="#">Sobre mi</a>
+            <a href="#">Contacto</a>
+          </nav>
+          <div>
             <DarkModeToggler />
           </div>
-        </div>
-      </ThemeProvider>
-    </>
+        </header>
+        <main className="flex flex-col items-center justify-center py-20 px-4"></main>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/DarkModeToggler.tsx
+++ src/components/DarkModeToggler.tsx
@@ -4,22 +4,23 @@ import { motion } from 'framer-motion';
 const DarkModeToggler = () => {
   const { darkMode, setDarkMode } = useDarkMode();
 
-  const shadowClass = darkMode
-    ? 'shadow-[inset_-4px_-4px_8px_rgba(0,0,0,0.2),inset_4px_4px_8px_rgba(0,0,0,0.7)]'
-    : 'shadow-[inset_-4px_-4px_8px_rgba(255,255,255,0.2),inset_-4px_4px_8px_rgba(0,0,0,0.7)]';
+  // const shadowClass = darkMode
+  //   ? 'shadow-[inset_12px_-4px_16px_rgba(0,0,0,0.5),inset_8px_2px_8px_rgba(0,0,0,0.6)]'
+  //   : 'shadow-[inset_-12px_4px_16px_rgba(20,20,20,0.4),inset_-8px_-2px_8px_rgba(20,20,20,0.4)]';
 
-  const outlineClass = darkMode ? 'outline outline-1 outline-gray-800' : 'outline outline-1 outline-gray-500';
+  // const outlineClass = darkMode ? 'outline-2 outline-gray-700' : 'outline-2 outline-gray-600';
 
   return (
     <motion.button
       onClick={() => setDarkMode(!darkMode)}
       aria-label="Toggle Dark Mode"
+      whileTap={{ scale: 0.95 }}
       className={`cursor-pointer w-14 h-8 flex items-center rounded-full p-1 transition-all duration-300
-        bg-gray-300 dark:bg-gray-700 box-border
-        ${shadowClass} ${outlineClass}`}
+  bg-gray-2s00  box-border
+  outline-2 outline-gray-600`}
     >
       <motion.div
-        className="w-6 h-6 bg-gray-700 dark:bg-gray-300 rounded-full shadow-md flex items-center justify-center"
+        className="w-6 h-6 bg-gray-700 dark:bg-gray-700 rounded-full shadow-md flex items-center justify-center"
         layout
         transition={{ type: 'spring', stiffness: 500, damping: 30 }}
         animate={{
